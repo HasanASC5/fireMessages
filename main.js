6a43ea2e09347f2a1735ae50b05160d7cb219e99
Hasan Khan <41080165+HasanASC5@users.noreply.github.com>
2018-08-09 09:13:23 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+const database = firebase.database().ref();
+database.on('child_added', displayMessageOnBoard);
+
+
+
+
+const usernameElement = document.getElementById("username");
+const messageElement = document.getElementById("message");
+const button = document.getElementById("submitButton");
+button.addEventListener("click",updateDB);
+
+//Set database object here
+
+
+/**
+ * Updates the database with the username and message.
+ */
+function updateDB(event){
+    event.preventDefault();
+    const username        = usernameElement.value;
+    const message         = messageElement.value;
+
+    usernameElement.value = "";
+    messageElement.value  = "";
+
+    const userData = {
+        NAME: username,
+        MESSAGE: message
+    }
+
+database.push(userData);
+
+    //Update database here
+
+}
+
+// Set database "child_added" event listener here
+
+function displayMessageOnBoard(rowData){
+    const row = rowData.val();
+
+    console.log(row);
+
+    const messageBox = document.querySelector('.allMessages');
+    const newLine = document.createElement('br');
+    const name = document.createTextNode(row.NAME + ':');
+    const message = document.createTextNode(row.MESSAGE);
+
+    messageBox.appendChild(newLine);
+    messageBox.appendChild(name);
+    messageBox.appendChild(message);
+
+}
